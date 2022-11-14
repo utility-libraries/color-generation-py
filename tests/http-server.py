c3b6a15631d1c676863ef871a91592a5fc548f37
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2022-11-14 13:34:26 +0100

    created interactive tests

---
+++ tests/http-server.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+# -*- coding=utf-8 -*-
+r"""
+
+"""
+import os.path as p
+import re
+import webbrowser
+import http.server
+import color_generator
+
+
+# htmlTemplateName = 'single.html'
+htmlTemplateName = 'palette.html'
+
+
+with open(p.join(p.dirname(__file__), htmlTemplateName)) as file:
+    template = file.read()
+
+
+print("Modes:", ', '.join(color_generator.generator.generationModes()))
+color_mode = input("Mode: ")
+
+
+class RequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path != '/':
+            self.send_error(404)
+            return
+
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html; charset=utf-8')
+        self.end_headers()
+
+        response = re.sub(re.escape('{color}'), lambda *a: color_generator.generate(color_mode).hex, template)
+
+        self.wfile.write(response.encode())
+
+
+server = http.server.HTTPServer(('', 8080), RequestHandler)
+
+if __name__ == '__main__':
+    webbrowser.open('http://localhost:8080/', 2)
+    server.serve_forever()
